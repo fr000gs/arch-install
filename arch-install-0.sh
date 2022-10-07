8966252fe5ab24fec898b19975247cd00e761d9a
fr000gs <103935614+fr000gs@users.noreply.github.com>
2022-10-07 15:29:57 +0530

    Create arch-install-0.sh

---
+++ arch-install-0.sh
@@ -0,0 +1,17 @@
+printf '\033c'
+
+echo "Welcome to fr000gs Arch installer."
+
+# Change ParallelDownloads from "5" to "10"
+sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
+# Update archlinux-keyring to avoid unnecessary errors
+pacman --noconfirm -Sy archlinux-keyring
+# Load US keyboard layout
+loadkeys us
+# Fix date and time
+timedatectl set-ntp true
+
+# Select drive to partition
+lsblk
+
+echo "Partition your drive, then run bash arch-install-1.sh"
